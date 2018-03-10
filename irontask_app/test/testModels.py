135d9619e13bd2fdf6be0dbf879e774641116c9b
Had <hhoutmann@gmail.com>
2018-03-10 18:55:48 +0100

    Ajout du dossier static pour les bibliothéques Javascript et bootstrap

---
+++ irontask_app/test/testModels.py
@@ -0,0 +1,10 @@
+from django.test import TestCase
+from irontask_app.models import Triathlon
+import unittest
+from django.core.urlresolvers import reverse
+from django.test import Client
+from irontask_app.models import Triathlon
+from django.contrib.auth.models import User
+from django.contrib.auth.models import Group
+from django.contrib.contenttypes.models import ContentType
+
