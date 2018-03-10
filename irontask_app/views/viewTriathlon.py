587960062385fd4f2be6dbc4034b845a1c884503
Had <hhoutmann@gmail.com>
2018-03-10 19:52:56 +0100

    Merde

--- irontask_app/views/viewTriathlon.py
+++ irontask_app/views/viewTriathlon.py
@@ -1,7 +1,21 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
-from irontask_app.models import T
+from irontask_app.models import Triathlon
 
 
 def listTriathlon(request):
-    return 0
+    """Vue qui retourne la liste de tous les triathlons"""
+
+    listTriathlon = Triathlon.objets.all()
+
+    return render(request, 'triatlon.html', {'triathlon': listTriathlon})
+
+
+def getTriathlon(request, id):
+    """
+    Vue qui retourne le triatlon fournit en paramètre
+    ::param id est l'id d'un triathlon
+    """
+    triathlon = Triathlon.objets.get(id=id)
+
+    return render(request, "triatlon.html", {'triathlon': triathlon})
