00ca9508bacc6bd885f626a792f5bed2fe53e219
Had <hhoutmann@gmail.com>
2018-03-11 17:02:41 +0100

    Implémentation du models sponsor

--- irontask_app/models.py
+++ irontask_app/models.py
@@ -28,3 +28,22 @@ class Triathlon(models.Model):
         (utile pour la partie admin) """
 
         return 'Triathlon du ' + self.date + ' à ' + self.ville
+
+
+class Sponsor(models.Model):
+    """
+    Class Réprensentant un sponsor
+    """
+    siret = models.CharField(max_length=14, primary_key=True, verbose_name='Siret', blank=False, null=False)
+    raisonSocial = models.CharField(max_length=50, verbose_name='Raison Sociale', blank=False, null=False)
+    adresse = models.CharField(max_length=50, blank=False, null=False)
+    codePostal = models.CharField(max_length=5, blank=False, null=False)
+    ville = models.CharField(max_length=50, blank=False, null=False)
+    telephone = models.CharField(max_length=10, blank=False, null=False)
+    email = models.EmailField
+
+    def __str__(self):
+        """
+        :return: String représentant l'objet Sponsor
+        """
+        return self.raisonSocial
