f240d2aef4e9a67e511d72f021c1a6e3314bd0da
Had <hhoutmann@gmail.com>
2018-03-12 18:45:38 +0100

    p

--- IronTask/settings.py
+++ IronTask/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'irontask_app.apps.IrontaskAppConfig',
-    #'rest_framework',
+
 ]
 
 MIDDLEWARE = [
--- irontask_app/urls.py
+++ irontask_app/urls.py
@@ -2,13 +2,11 @@ from django.contrib import admin
 from django.urls import path, include
 from irontask_app.views import viewSponsor
 from django.conf.urls import url, include
-from rest_framework import routers
+
 from irontask_app import api
 from irontask_app import views
 
-"""router = routers.DefaultRouter()
 
-router.register(r'sponsor', api.SponsorViewSet)"""
 
 
 
