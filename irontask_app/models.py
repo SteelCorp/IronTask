90bed4e598413b7e7d52b9bb06f572e054355b55
SebPou <sebastien.poussard@protonmail.ch>
2018-03-12 12:44:12 +0100

    Ajout class intervenant dans models
    modficiation de la classe sponsor et 
    ajout de la classe intervenant dans models.py

--- irontask_app/models.py
+++ irontask_app/models.py
@@ -6,6 +6,18 @@ from django.core.validators import RegexValidator
 
 
 
+class Intervenant(models.Model):
+    """
+    Class Réprensentant un intervenant
+    """
+    siret = models.PositiveIntegerField(max_length=14, primary_key=True)
+    raisonSocial = models.CharField(max_length=50, verbose_name='Raison Sociale', blank=False, null=False)
+    type = models.CharField(max_length=50, blank=False, null=False)
+    adresse = models.CharField(max_length=200, blank=False, null=False)
+    codePostal = models.PositiveSmallIntegerField(max_length=5, blank=False, null=False)
+    ville = models.CharField(max_length=50, blank=False, null=False)
+    telephone = models.CharField(max_length=12, blank=False, null=False)
+    email = models.EmailField()
 
 
 class Sponsor(models.Model):
@@ -13,13 +25,14 @@ class Sponsor(models.Model):
     Class Réprensentant un sponsor
     """
 
-    siret = models.IntegerField(max_length=14, primary_key=True, verbose_name='Siret', blank=False, null=False)
+    siret = models.PositiveIntegerField(max_length=14, primary_key=True)
     raisonSocial = models.CharField(max_length=50, verbose_name='Raison Sociale', blank=False, null=False)
-    adresse = models.CharField(max_length=50, blank=False, null=False)
+    adresse = models.CharField(max_length=200, blank=False, null=False)
     codePostal = models.CharField(max_length=5, blank=False, null=False)
     ville = models.CharField(max_length=50, blank=False, null=False)
     telephone = models.CharField(max_length=12, blank=False, null=False)
     email = models.EmailField()
+
 
 
     def __str__(self):
