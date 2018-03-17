c305a12f3e260c0dcd481e71978fdb4f2010b931
Had <hhoutmann@gmail.com>
2018-03-17 14:52:55 +0100

    Mise en commentaire pagination config

--- irontask_app/models.py
+++ irontask_app/models.py
@@ -11,7 +11,7 @@ class UserProfile(models.Model):
     Class pour la gestion des utilateurs du logiciel (connexion etc...)
     """
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    pagination_config = models.IntegerField(default=25)
+    #pagination_config = models.IntegerField(default=25)
 
 
 
