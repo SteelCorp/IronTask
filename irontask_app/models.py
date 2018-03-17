8fc5c2e22d16e67a7244b7a918edba01da9547c2
Had <hhoutmann@gmail.com>
2018-03-17 14:11:25 +0100

    Models

--- irontask_app/models.py
+++ irontask_app/models.py
@@ -6,6 +6,98 @@ import datetime
 
 # Create your models here.
 
+class UserProfile(models.Model):
+    """
+    Class pour la gestion des utilateurs du logiciel (connexion etc...)
+    """
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+
+
+class Intervenant(models.Model):
+    """
+    Class Reprénsentant un intervenant
+    """
+    siret = models.PositiveIntegerField(max_length=14, primary_key=True)
+    raisonSocial = models.CharField(max_length=50, verbose_name='Raison Sociale', blank=False, null=False)
+    type = models.CharField(max_length=50, blank=False, null=False)
+    adresse = models.CharField(max_length=200, blank=False, null=False)
+    codePostal = models.PositiveSmallIntegerField(max_length=5, blank=False, null=False)
+    ville = models.CharField(max_length=50, blank=False, null=False)
+    telephone = models.CharField(max_length=12, blank=False, null=False)
+    email = models.EmailField()
+
+    def __str__(self):
+        """
+        :return: String représentant l'objet Intervenant par sa raison social
+        """
+        return self.raisonSocial
+
+
+class Sponsor(models.Model):
+    """
+    Class Reprénsentant un sponsor
+    """
+
+    siret = models.CharField(max_length=14, primary_key=True)
+    raisonSocial = models.CharField(max_length=50, verbose_name='Raison Sociale', blank=False, null=False)
+    adresse = models.CharField(max_length=200, blank=False, null=False)
+    codePostal = models.CharField(max_length=5, blank=False, null=False)
+    ville = models.CharField(max_length=50, blank=False, null=False)
+    telephone = models.CharField(max_length=12, blank=False, null=False)
+    email = models.EmailField()
+
+    def __str__(self):
+        return self.raisonSocial
+
+
+
+class Categorie(models.Model):
+    """
+    Class Reprénsentant une catégorie de triathlon
+
+    age max peut etre nullable car une catégorie peux etre ouverte à toute age
+    sexe est représenté sous forme de booléen pas soucie d'optimisation
+    """
+
+    SEX_CHOICES = (
+        ('F', 'Feminin',),
+        ('M', 'Masculin',),
+
+    )
+
+    libelle = models.CharField(max_length=50, blank=False, null=False)
+    ageMin = models.PositiveSmallIntegerField(blank=False, null=False)
+    ageMax = models.PositiveSmallIntegerField()
+    sexe = models.BooleanField(choices=SEX_CHOICES)
+
+    def __str__(self):
+        """
+        :return: String représentant l'objet Catégorie par son libellé
+        """
+        return self.libelle
+
+
+class Materiel(models.Model):
+    """
+    Class Représentant le materiel
+    """
+
+    nom = models.CharField(max_length=50, blank=False, null=False)
+    type = models.CharField(max_length=50, blank=False, null=False)
+    qteTotal = models.PositiveIntegerField(blank=False, null=False)
+    lieuStockage = models.CharField(max_length=50, blank=False, null=False)
+
+    def __str__(self):
+        """
+        :return: String représentant l'objet Materiel par son nom et son lieu de stockage
+        """
+        return self.nom + ' stocké a : ' + self.lieuStockage
+
+
+
+# Create your models here.
+
 class Triathlon(models.Model):
     """Class Répresentant un Triathlon"""
 
