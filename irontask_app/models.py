7f355201461bd3e3d8deeb4450d0a9c3eea1393b
Had <hhoutmann@gmail.com>
2018-03-18 11:15:12 +0100

    Correctio des erreurs sur intervenant

--- irontask_app/models.py
+++ irontask_app/models.py
@@ -19,11 +19,11 @@ class Intervenant(models.Model):
     """
     Class Reprénsentant un intervenant
     """
-    siret = models.PositiveIntegerField(max_length=14, primary_key=True)
+    siret = models.CharField(max_length=14, primary_key=True)
     raisonSocial = models.CharField(max_length=50, verbose_name='Raison Sociale', blank=False, null=False)
     type = models.CharField(max_length=50, blank=False, null=False)
     adresse = models.CharField(max_length=200, blank=False, null=False)
-    codePostal = models.PositiveSmallIntegerField(max_length=5, blank=False, null=False)
+    codePostal = models.CharField(max_length=5, blank=False, null=False)
     ville = models.CharField(max_length=50, blank=False, null=False)
     telephone = models.CharField(max_length=12, blank=False, null=False)
     email = models.EmailField()
