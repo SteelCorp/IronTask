0d1a0bed6456e1f1541506537857d8cc5df6115c
Digo <romero.diego31@gmail.com>
2018-03-20 22:42:26 +0100

    Deplacement classe tache
    Deplacement classe tache pour pouvoir l'appeler dans le triathlon dans la foreignkey

--- irontask_app/models.py
+++ irontask_app/models.py
@@ -98,35 +98,6 @@ class Materiel(models.Model):
 
 
 
-
-class Triathlon(models.Model):
-    """Class Répresentant un Triathlon"""
-
-    date = models.DateField(null=False, blank=False, verbose_name='date')
-
-    heureDepart = models.TimeField(null=False, blank=False, verbose_name='heureDepart')
-
-    codePostal = models.CharField(max_length=5, null=False, blank=False, verbose_name='codePostal')
-
-    adresse = models.CharField(max_length=50, null=False, blank=False, verbose_name='adresse')
-
-    ville = models.CharField(max_length=50, null=False, blank=False, verbose_name='ville')
-
-   # taches = models.ForeignKey(Tache, on_delete=models.CASCADE)
-    # à coder la class Tache pour referencer la clé étrangère
-
-    #typeTriathlon = models.ForeignKey(TypeTriathlon, on_delete=models.PROTECT)
-
-    # à coder la class TypeTriathlon pour referencer la clé étrangère
-
-    def __str__(self):
-        """Retourne une représentation string de l'objet Triathlon
-        (utile pour la partie admin) """
-
-        return 'Triathlon du ' + self.date + ' à ' + self.ville
-
-
-
 class Tache(models.Model):
     """Class Representant une Tache"""
 
@@ -150,5 +121,33 @@ class Tache(models.Model):
         """Retrourne une representation string de l'objet Tache"""
 
         return 'Tache date de fin :' + self.DateFin + ' avec niveau d\'avancement :' + self.NiveauAvancement
+
+
+class Triathlon(models.Model):
+    """Class Répresentant un Triathlon"""
+
+    date = models.DateField(null=False, blank=False, verbose_name='date')
+
+    heureDepart = models.TimeField(null=False, blank=False, verbose_name='heureDepart')
+
+    codePostal = models.CharField(max_length=5, null=False, blank=False, verbose_name='codePostal')
+
+    adresse = models.CharField(max_length=50, null=False, blank=False, verbose_name='adresse')
+
+    ville = models.CharField(max_length=50, null=False, blank=False, verbose_name='ville')
+
+    taches = models.ForeignKey(Tache, on_delete=models.CASCADE)
+    # à coder la class Tache pour referencer la clé étrangère
+
+    #typeTriathlon = models.ForeignKey(TypeTriathlon, on_delete=models.PROTECT)
+
+    # à coder la class TypeTriathlon pour referencer la clé étrangère
+
+    def __str__(self):
+        """Retourne une représentation string de l'objet Triathlon
+        (utile pour la partie admin) """
+
+        return 'Triathlon du ' + self.date + ' à ' + self.ville
+
 
 
