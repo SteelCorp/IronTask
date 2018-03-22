305915f767f29f3a58326dc4de7956eaf1fa3a05
ForT <julien.trottet@gadz.org>
2018-03-22 16:56:00 +0100

    Suite

--- irontask_app/models.py
+++ irontask_app/models.py
@@ -4,15 +4,12 @@ from django.contrib.auth.models import User
 import datetime
 
 
-
-
 class UserProfile(models.Model):
     """
     Class pour la gestion des utilateurs du logiciel (connexion etc...)
     """
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    #pagination_config = models.IntegerField(default=25)
-
+    # pagination_config = models.IntegerField(default=25)
 
 
 class Intervenant(models.Model):
@@ -20,13 +17,14 @@ class Intervenant(models.Model):
     Class Reprénsentant un intervenant
     """
     siret = models.CharField(max_length=14, primary_key=True)
-    raisonSocial = models.CharField(max_length=50, verbose_name='Raison Sociale', blank=False, null=False)
-    type = models.CharField(max_length=50, blank=False, null=False)
-    adresse = models.CharField(max_length=200, blank=False, null=False)
-    codePostal = models.CharField(max_length=5, blank=False, null=False)
-    ville = models.CharField(max_length=50, blank=False, null=False)
-    telephone = models.CharField(max_length=12, blank=False, null=False)
-    email = models.EmailField()
+    raisonSocial = models.CharField(max_length=50, blank=False, null=True)
+    type = models.CharField(max_length=50, blank=False, null=True)
+    adresse = models.CharField(max_length=200, blank=False, null=True)
+    codePostal = models.CharField(max_length=5, blank=False, null=True)
+    ville = models.CharField(max_length=50, blank=False, null=True)
+    telephoneFixe = models.CharField(max_length=10, blank=False, null=True)
+    telephonePort = models.CharField(max_length=10, blank=False, null=True)
+    email = models.EmailField(blank=False, null=False)
 
     def __str__(self):
         """
@@ -41,16 +39,16 @@ class Sponsor(models.Model):
     """
 
     siret = models.CharField(max_length=14, primary_key=True)
-    raisonSocial = models.CharField(max_length=50, verbose_name='Raison Sociale', blank=False, null=False)
-    adresse = models.CharField(max_length=200, blank=False, null=False)
-    codePostal = models.CharField(max_length=5, blank=False, null=False)
-    ville = models.CharField(max_length=50, blank=False, null=False)
-    telephone = models.CharField(max_length=12, blank=False, null=False)
-    email = models.EmailField()
+    raisonSocial = models.CharField(max_length=50, blank=False, null=True)
+    adresse = models.CharField(max_length=200, blank=False, null=True)
+    codePostal = models.CharField(max_length=5, blank=False, null=True)
+    ville = models.CharField(max_length=50, blank=False, null=True)
+    telephoneFixe = models.CharField(max_length=10, blank=False, null=True)
+    telephonePort = models.CharField(max_length=10, blank=False, null=True)
+    email = models.EmailField(blank=False, null=True)
 
     def __str__(self):
         return self.raisonSocial
-
 
 
 class Categorie(models.Model):
@@ -66,11 +64,10 @@ class Categorie(models.Model):
         ('M', 'Masculin',),
 
     )
-
     libelle = models.CharField(max_length=50, blank=False, null=False)
     ageMin = models.PositiveSmallIntegerField(blank=False, null=False)
-    ageMax = models.PositiveSmallIntegerField()
-    sexe = models.BooleanField(choices=SEX_CHOICES)
+    ageMax = models.PositiveSmallIntegerField(blank=False, null=False)
+    sexe = models.CharField(max_length=1, choices=SEX_CHOICES)
 
     def __str__(self):
         """
@@ -96,113 +93,137 @@ class Materiel(models.Model):
         return self.nom + ' stocké a : ' + self.lieuStockage
 
 
+class Benevole(models.Model):
+    """Class Representant un bénévole"""
+    SEX_CHOICES = (
+        ('Feminin', 'F',),
+        ('Masculin', 'M',),
 
-
-class Tache(models.Model):
-    """Class Representant une Tache"""
-
-    NivPriorite = (
-        ('0', 'Aucune'),
-        ('1', 'Faible'),
-        ('2', 'Moyen'),
-        ('3', 'Important')
     )
-    DateFin = models.DateField(null=False, blank=False, verbose_name='DateFin')
-
-    NiveauAvancement = models.CharField(max_length=1, choices=NivPriorite)
-
-    Triathlon = models.ForeignKey(Triathlon, on_delete=models.PROTECT)
-
-    #Benevole = models.ForeignKey(Benevole, on_delete=models.PROTECT)
-
-    # TacheModel = models.ForeignKey(TacheModel, on_delete=models.PROTECT)
-
-    def __str__(self):
-        """Retrourne une representation string de l'objet Tache"""
-
-        return 'Tache date de fin :' + self.DateFin + ' avec niveau d\'avancement :' + self.NiveauAvancement
-
-
-class TypeTriathlon(models.Model):
-    """Class representant un type de triathlon"""
-
-    # id???
-
-    libelle = models.CharField(max_length=50, blank=False, null=False)
-
-    distanceNatation = models.PositiveIntegerField(blank=False, null=False)
-
-    distanceCyclisme = models.PositiveIntegerField(blank=False, null=False)
-
-    distanceCourseAPied = models.PositiveIntegerField(blank=False, null=False)
-
-    # triathlon = models.ForeignKey(Triathlon, on_delete=) ???
-
-    def __str__(self):
-        """Retourne un string des parametre du type de triathlon"""
-        return 'Type triathlon de libelle ' + self.libelle + ' Natation' + self.distanceNatation + ' Cyclisme ' + self.distanceCyclisme + ' Course ' + self.distanceCourseAPied
-
-
-class Triathlon(models.Model):
-    """Class Répresentant un Triathlon"""
-
-    date = models.DateField(null=False, blank=False, verbose_name='date')
-
-    heureDepart = models.TimeField(null=False, blank=False, verbose_name='heureDepart')
-
-    codePostal = models.CharField(max_length=5, null=False, blank=False, verbose_name='codePostal')
-
-    adresse = models.CharField(max_length=50, null=False, blank=False, verbose_name='adresse')
-
-    ville = models.CharField(max_length=50, null=False, blank=False, verbose_name='ville')
-
-    taches = models.ForeignKey(Tache, on_delete=models.CASCADE)
-    # à coder la class Tache pour referencer la clé étrangère
-
-    typeTriathlon = models.ForeignKey(TypeTriathlon, on_delete=models.PROTECT)
-    # à coder la class TypeTriathlon pour referencer la clé étrangère
-
-    def __str__(self):
-        """Retourne une représentation string de l'objet Triathlon
-        (utile pour la partie admin) """
-
-        return 'Triathlon du ' + self.date + ' à ' + self.ville
-
-
-
-class Tache(models.Model):
-    """Class Representant une Tache"""
-
-    STATUS_CHOICES =(
+    STATUS_CHOICES = (
         ('A', 'Administrateur'),
         ('O', 'Organisateur'),
         ('B', 'Benevole'),
     )
 
-    Nom = models.CharField(max_length=50, blank=False, null=False, verbose_name='Nom')
-
-    Prenom = models.CharField(max_length=50, blank=False, null=False,verbose_name='Prenom')
-
-    DateNaissance = models.DateField(blank=False, null=False, verbose_name='Date de naissance')
-
-    Sexe = models.BooleanField(choices=SEX_CHOICES,verbose_name='Sexe')
-
-    Adresse = models.CharField(max_length=50, blank=False, null=False,verbose_name='Adresse')
-
-    CodePostal = models.CharField(max_length=5, blank=False, null=False, verbose_name='Code Postal')
-
-    Ville = models.CharField(max_length=50, blank=False, null=False,verbose_name='Ville')
-
-    TelephoneFixe = models.CharField(max_length=10, blank=False, null=False, verbose_name='Telephone Fixe')
-
-    TelephonePortable = models.CharField(max_length=10, blank=False, null=False, verbose_name='Telephone Portable')
-
-    Email = models.EmailField(verbose_name='Email')
-
-    Status = models.CharField(max_length=1, choices=STATUS_CHOICES,verbose_name='Status')
+    nom = models.CharField(max_length=50, blank=False, null=False)
+    prenom = models.CharField(max_length=50, blank=False, null=False)
+    dateNaissance = models.DateField(blank=False, null=True)
+    sexe = models.CharField(choices=SEX_CHOICES, max_length=30)
+    adresse = models.CharField(max_length=50, blank=False, null=True)
+    codePostal = models.CharField(max_length=5, blank=False, null=True)
+    ville = models.CharField(max_length=50, blank=False, null=True)
+    telephoneFixe = models.CharField(max_length=10, blank=False, null=True)
+    telephonePortable = models.CharField(max_length=10, blank=False, null=False)
+    email = models.EmailField(blank=False, null=False)
+    status = models.CharField(max_length=1, choices=STATUS_CHOICES, null=False)
 
     def __str__(self):
         """String du benevole (lol)"""
-        return 'Benevole :' +self.Nom+" "+self.Prenom+' Status'+ self.STATUS_CHOICES
+        return 'Benevole :' + self.nom + " " + self.prenom + ' Status' + self.status
 
-    
+
+class TypeTriathlon(models.Model):
+    """Class representant un type de triathlon"""
+
+    libelle = models.CharField(max_length=50, blank=False, null=False)
+    distanceNatation = models.PositiveIntegerField(blank=False, null=False)
+    distanceCyclisme = models.PositiveIntegerField(blank=False, null=False)
+    distanceCourseAPied = models.PositiveIntegerField(blank=False, null=False)
+
+    def __str__(self):
+        """Retourne un string des parametre du type de triathlon"""
+        return 'Type triathlon de libelle ' + self.libelle
+
+
+class Triathlon(models.Model):
+    """Class Répresentant un Triathlon"""
+
+    date = models.DateField(null=False, blank=False)
+    heureDepart = models.TimeField(null=False, blank=False)
+    codePostal = models.CharField(max_length=5, null=False, blank=False)
+    adresse = models.CharField(max_length=50, null=False, blank=False)
+    ville = models.CharField(max_length=50, null=False, blank=False)
+    fk_TypeTriathlon = models.ForeignKey(TypeTriathlon, on_delete=models.PROTECT,null=False)
+
+    def __str__(self):
+        """Retourne une représentation string de l'objet Triathlon
+        (utile pour la partie admin) """
+
+        return 'Triathlon du ' + str(self.date) + ' à ' + self.ville
+
+
+class Tache(models.Model):
+    """Class Representant une Tache"""
+    """
+    Fusion avec tache modèle
+    """
+    NIV_AVANCEMENT = (
+        ('P', 'Plannifié'),
+        ('E', 'En cours'),
+        ('V', 'Validé'),
+    )
+    NIV_PRIOROTE = (
+        ('0', 'Aucune'),
+        ('1', 'Faible'),
+        ('2', 'Moyen'),
+        ('3', 'Important')
+    )
+    nomTache = models.CharField(max_length=150)
+    description = models.CharField(max_length=500)
+    dateDebut = models.DateField(null=False, blank=False)
+    dateFin = models.DateField(null=False, blank=False)
+    dateRappel = models.DateField(null=True, blank=False)
+    niveauAvancement = models.CharField(max_length=1, choices=NIV_AVANCEMENT)
+    niveauPriorite = models.CharField(max_length=1, choices=NIV_PRIOROTE)
+    fk_triathlon = models.ForeignKey(Triathlon, on_delete=models.PROTECT, null=False)
+    fk_benevole = models.ForeignKey(Benevole, on_delete=models.PROTECT, null=False)
+
+    def __str__(self):
+        """Retrourne une representation string de l'objet Tache"""
+
+        return 'Tache date de fin :' + str(self.dateFin) + ' avec niveau d\'avancement :' + self.niveauAvancement
+
+
+class Intervenir(models.Model):
+    """Class Representant le lien entre triathlon et Intervenant"""
+
+    devis = models.CharField(max_length=150)
+    prixDevis=models.PositiveIntegerField(max_length=6, null=False, blank=False)
+    fk_triathlon = models.ForeignKey(Triathlon, on_delete=models.CASCADE, null=False)
+    fk_intervenant = models.ForeignKey(Intervenant, on_delete=models.CASCADE, null=False)
+
+
+class Sponsoriser(models.Model):
+    """Class Representant le lien entre triathlon et Sponsor"""
+
+    donation=models.PositiveIntegerField(max_length=6, null=False, blank=False)
+    fk_triathlon = models.ForeignKey(Triathlon, on_delete=models.CASCADE, null=False)
+    fk_sponsoriser = models.ForeignKey(Sponsor, on_delete=models.CASCADE, null=False)
+
+
+class Caracteriser(models.Model):
+    """Class Representant le lien entre triathlon et catégorie (exemple 20 filles seniors pour triat Lyon"""
+
+    nbrParticipant=models.PositiveIntegerField(max_length=6, null=False, blank=False)
+    fk_triathlon = models.ForeignKey(Triathlon, on_delete=models.CASCADE, null=False)
+    fk_categorie = models.ForeignKey(Categorie, on_delete=models.CASCADE, null=False)
+
+
+class Allouer(models.Model):
+    """Class Representant le lien caracterisant l'allocation d'un materiel pour un triathlon donné"""
+
+    qteUtilise=models.PositiveIntegerField(max_length=6, null=False, blank=False)
+    fk_triathlon = models.ForeignKey(Triathlon, on_delete=models.CASCADE, null=False)
+    fk_materiel = models.ForeignKey(Materiel, on_delete=models.CASCADE, null=False)
+    fk_benevole = models.ForeignKey(Benevole, on_delete=models.CASCADE, null=False)
+
+    class Meta:
+        unique_together = (('fk_triathlon', 'fk_benevole'),)
+
+
+class Affecter (models.Model):
+    """Class Represantant le lien caractérisant entre benevole et tache"""
+
+    fk_benevole = models.ForeignKey(Benevole, on_delete=models.CASCADE, null=False, blank=False)
+    fk_tache = models.ForeignKey(Tache, on_delete=models.CASCADE,blank=False,null=False)
