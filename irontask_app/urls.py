9e5e67624b4a658c0583e40f58a3b9b654356fc9
Had <hhoutmann@gmail.com>
2018-05-26 13:52:41 +0200

    Implémentation du calendrier

--- irontask_app/urls.py
+++ irontask_app/urls.py
@@ -56,10 +56,12 @@ urlpatterns = [
     # Gestion des taches
     path('tache/liste/', viewTache.listTache, name='listTache'),
     path('tache/get/<id>/', viewTache.getTache, name='getTache'),
-    path('tache/calendrier/', viewTache.listTache, name='listTacheCal'),
+    path('tache/calendrier/', viewTache.calendrierTache, name='listTacheCal'),
     path('tache/liste/supprimer/<id>', viewTache.deleteTache, name='deleteTache'),
     path('tache/liste/editer/<id>', viewTache.editerTache, name='editerTache'),
     path('tache/ajouter/', viewTache.ajouterTache, name='ajouterTache'),
+
+
 
     #Gestion des affectations
     path('affecter/supprimer/<pk>/', viewAffecter.supprimer, name='supprimerAffectation'),
